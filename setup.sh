1a7cb311c47ad9513439547b7bd5d3bcb4695a25
wulan17 <galihgustip@gmail.com>
2020-11-28 19:30:37 +0700

    cleanup
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,28 +1,7 @@
 #!/bin/bash
 export curr=$(dirname $(realpath $0))
 export url="https://github.com/wulan17/zsh-config/raw/master"
-export os=$(uname -o)
-function select_distro(){
-	echo "Select Your Operation System :"
-	echo "	1. Ubuntu, Debian, Linux Mint
-	2. Arch Linux, Manjaro"
-	echo -n "Choose : "
-	read distro
-	echo ""
-	echo "Installing zsh..."
-	case "$distro" in
-		1)
-			sudo apt update
-			sudo apt install curl zsh -y
-			;;
-		2)
-			sudo pacman -Sy curl zsh
-			;;
-		*)
-			exit
-			;;
-	esac
-}
+
 function select_antigen(){
 	echo "Choose antigen type :"
 	echo "	1. Stable
@@ -50,31 +29,10 @@ function setup(){
 		cp "$curr"/{.zshrc,.antigenrc} ~/
 	fi
 }
-
-if [ $os == "Android" ];then
-	echo "Installing zsh..."
-	pkg update -y
-	pkg install curl zsh debianutils -y
-	select_antigen
-	setup
-	if [ ! -d ~/.termux ];then
-		mkdir ~/.termux
-	fi
-	if [ -z $(ls "$curr"/termux.properties) ];then
-		curl -L "$url"/termux.properties > .termux/termux.properties
-	else
-		cp "$curr"/termux.properties ~/.termux/termux.properties
-	fi
-	echo "Set default shell..."
-	chsh -s zsh
-	echo "Done"
-	zsh
-else
-	select_distro
-	select_antigen
-	setup
-	echo "Set default shell..."
-	sudo chsh $(whoami) -s $(which zsh)
-	echo "Done"
-	zsh
-fi
+sudo pacman -Sy curl zsh
+select_antigen
+setup
+echo "Set default shell..."
+sudo chsh $(whoami) -s $(which zsh)
+echo "Done"
+zsh
